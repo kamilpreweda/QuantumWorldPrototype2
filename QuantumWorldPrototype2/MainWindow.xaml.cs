8b4fe292e8cbf85b8d1c808fd7e69ce1fea2255b
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-11 12:59:25 +0200

    Added template for Overview and Buildings views using User Control

--- QuantumWorldPrototype2/MainWindow.xaml.cs
+++ QuantumWorldPrototype2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuantumWorldPrototype2.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,17 @@ namespace QuantumWorldPrototype2
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new OverviewViewModel();
+        }
+
+        private void Buildings_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new BuildingsViewModel();
+        }
+
+        private void Overview_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new OverviewViewModel();
         }
     }
 }
